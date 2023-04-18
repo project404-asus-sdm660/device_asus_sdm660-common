dbaa7483fd34a93184db7594d37f81451b2403d3
jmpfbmx <jmpf.bmx@gmail.com>
2023-04-18 21:23:57 +0530

    sdm660-common: camera: Build libyuv
     * Could not load /vendor/lib/hw/android.hardware.camera.provider@2.4-impl.so from default namespace: dlopen failed: library "libyuv.so" not found: needed by /vendor/lib/camera.device@3.4-external-impl.so in namespace (default).
     * Could not get passthrough implementation for android.hardware.camera.provider@2.4::ICameraProvider/legacy/0.
    
    Signed-off-by: karthik1896 <karthik1896@outlook.com>

--- sdm660.mk
+++ sdm660.mk
@@ -65,7 +65,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0.vendor \
-    libxml2
+    libxml2 \
+    libyuv
 
 PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.camera.eis.enable=1 \
